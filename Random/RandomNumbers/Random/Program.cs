87639680aaa3023faf803683a15b905aec3502ba
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2021-03-30 02:18:32 +0200

    generar numeros únicos y aleatorios

---
+++ Random/RandomNumbers/Random/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RandomMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Int32> Numbers = new List<Int32>();
+            Random random = new Random();
+
+            for (int i = 0; i < 100001; i++)
+            {
+                var number = random.Next(Int32.MinValue, Int32.MaxValue);
+                if (!Numbers.Contains((number)))
+                {
+                    Numbers.Add(number);
+                }
+
+            }             
+
+            PrintFile(Numbers);
+        }
+
+         
+
+        static void PrintFile(IEnumerable<int> numbers)
+        {
+            string path = @"C:\Users\je\Desktop\pruebaInercya\pruebaincercya\Random\numbers.txt"; 
+
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+
+                using (TextWriter tw = new StreamWriter(path))
+                {
+                    foreach (var number in numbers)
+                    {
+                        tw.WriteLine(number);
+                    }
+                    tw.Close();
+                }
+
+            }
+            else if (File.Exists(path))
+            {
+
+                using (var tw = new StreamWriter(path, false))
+                {
+                    foreach (var number in numbers)
+                    {
+                        tw.WriteLine(number);
+                    }
+                    tw.Close();
+                }
+            }
+        }
+    }
+}
+ 
